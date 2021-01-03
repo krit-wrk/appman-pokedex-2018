7b0d155d3de11f5768e976be95f78446c582fd10
Krit <>
2021-01-03 14:30:22 +0700

    add search all

--- src/server.js
+++ src/server.js
@@ -1,29 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const _ = require('lodash')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const _ = require("lodash");
+const app = express();
 
-const { cards } = require('./../mock/cards.json')
+const { cards } = require("./../mock/cards.json");
 
-app.use(cors())
+app.use(cors());
+app.use(function (req, res, next) {
+	setTimeout(next, 2000);
+});
 
-app.get('/api/cards', (req, res) => {
-  const { name, type, limit = 20 } = req.query
+app.get("/api/cards", (req, res) => {
+	const { name, type, query, limit = 20 } = req.query;
 
-  if (_.every([name, type], item => item === undefined)) {
-    return res.json({ cards: cards.slice(0, limit) })
-  }
+	if (_.every([name, type, query], (item) => item === undefined)) {
+		return res.json({ cards: cards.slice(0, limit) });
+	}
 
-  res.json({
-    cards: _.filter(cards, card => {
-      const name = _.toUpper(_.get(req, 'query.name', ''))
-      const type = _.toUpper(_.get(req, 'query.type', ''))
-      const checkName = _.includes(_.toUpper(card.name), name)
-      const checkType = _.includes(_.toUpper(card.type), type)
-      return checkName && checkType
-    })
-  })
+	if (query) {
+		return res.json({
+			cards: _.filter(cards, (card) => {
+				const queryUpper = _.toUpper(query);
+				const checkName = _.includes(_.toUpper(card.name), queryUpper);
+				const checkType = _.includes(_.toUpper(card.type), queryUpper);
+				return checkName || checkType;
+			}),
+		});
+	}
 
-})
+	res.json({
+		cards: _.filter(cards, (card) => {
+			const name = _.toUpper(_.get(req, "query.name", ""));
+			const type = _.toUpper(_.get(req, "query.type", ""));
+			const checkName = _.includes(_.toUpper(card.name), name);
+			const checkType = _.includes(_.toUpper(card.type), type);
+			return checkName && checkType;
+		}),
+	});
+});
 
-app.listen(3030, () => console.log('app start @ port 3030'))
+app.listen(3030, () => console.log("app start @ port 3030"));
